1126fc9d5799f4b5325936b2e5193ea33abbc2f3
routhuvivek <tmfapigithub@gmail.com>
2023-08-23 09:29:01 +0000

    TMFAPI-633 basic API calls v1.

--- spring-server/src/main/java/io/swagger/api/ServiceCategoryApi.java
+++ spring-server/src/main/java/io/swagger/api/ServiceCategoryApi.java
@@ -12,13 +12,7 @@ import io.swagger.model.ServiceCategoryUpdate;
 import io.swagger.annotations.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.Valid;
@@ -29,6 +23,7 @@ import java.util.List;
 @Validated
 @Api(value = "serviceCategory", description = "the serviceCategory API")
 @RequestMapping(value = "/tmf-api/serviceCatalogManagement/v4/")
+@CrossOrigin
 public interface ServiceCategoryApi {
 
     @ApiOperation(value = "Creates a ServiceCategory", nickname = "createServiceCategory", notes = "This operation creates a ServiceCategory entity.", response = ServiceCategory.class, tags={ "serviceCategory", })
